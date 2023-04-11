6218ef072b48a8158fa636cc06e6f5ecc3d9b2fa
zhongshijun <zsj499405764@qq.com>
2023-04-11 21:29:02 +0800

    Update aec1.c

--- aec1.c
+++ aec1.c
@@ -2,11 +2,11 @@
 void HighProc(float *gain)
 {
     int bins = 257;
-    const int FREQ_400HZ = 300;
+    const int FREQ_300HZ = 300;
     const int FREQ_1200HZ = 1200;
     const int FREQ_8000HZ = 8000;
 
-    int lowFreq = FREQ_400HZ / FREQ_8000HZ * bins;
+    int lowFreq = FREQ_300HZ / FREQ_8000HZ * bins;
     int highFreq = FREQ_1200HZ / FREQ_8000HZ * bins;
     int band = (highFreq - lowFreq) / 4 + 0.5;
 
@@ -47,8 +47,8 @@ float smoothERL[257];
 float smoothSER[257];
 
 for (int i = 0; i < 257; ++i) {
-    curERL[i] = dPowSmooth[i] / (xPowSmooth[i] + EPS);
-    curSER[i] = errEstPow[i] / (echoEstPow[i] + EPS);
+    curERL[i] = (dPowSmooth[i] - noiseEst[i]) / (xPowSmooth[i] + EPS);
+    curSER[i] = (errEstPow[i]  - noiseEst[i]) / (echoEstPow[i] + EPS);
 }
 
 SMOOTH(smoothERL, curERL, 0.98);
@@ -65,8 +65,8 @@ float snrEchoEst[257]; // 信回比
 copy(errEst, tmpErrEst, FFT_SIZE); // errEst->tmpErrEst
 
 IFFT(tmpErrEst);
-for (int i = 0; i < 257; ++i) {
-    tmpErrEst[2 * i] = max(tmpErrEst[2 * i], 0);
+for (int i = 0; i < 514; ++i) {
+    tmpErrEst[i] = max(tmpErrEst[i], 0);
 }
 FFT(tmpErrEst);
 
@@ -92,10 +92,31 @@ for (i = 0; i < 257; ++i) {
 
 
 // 4. 倒谱消回声
-for () {
+  // 分成0~4K低频做和4~8K
+  // cemp8k: 0~4K倒谱
+  // cemp16k: 4~8K倒谱
+  // echoEst: 回声估计
+  // 先求倒谱
+    for (i = 0; i < 123; ++i) {
+        tmpPow[i] = echoEst[2 * i] * echoEst[2 * i] + 
+                    echoEst[2 * i + 1] * echoEst[2 * i + 1];
+    }
 
+    tmpPow = Log(tmpPow);
 
-}
+    tmpPow[123] = 0;
+    for (i = 123; i >= 0; --i) {
+        tmpPow[2 * i ] = tmp[i];
+        tmpPow[2 * i + 1] = 0;
+    }
+
+    IFFT(tmpPow);
+
+    for (i = 0; i < 257; ++i) {
+        tmpPow[i] = max(tmpPow[i], 0);
+    }
+
+    cemp8k = FFT(tmpPow);
 
 // 5. 相干性计算
    X: 远端频域数据
@@ -112,7 +133,7 @@ for () {
    corr_xerr: 远端和残差互相关
    
   // cohxd
-  // 1. 计算分母能量
+  // a. 计算分母能量
   for (i = 0; i < 257; ++i) {
     X_power[i] = X[2 * i] * X[2 * i] + X[2 * i + 1] * X[2 * i + 1];
     Y_power[i] = Y[2 * i] * Y[2 * i] + Y[2 * i + 1] * Y[2 * i + 1];
@@ -120,24 +141,95 @@ for () {
     X_power_smooth[i] = 0.9 * X_power_smooth[i] + 0.1 * X_power[i];
   }
   
-  // 2. 互相关
+  // b. 互相关
   for (i = 0; i < 257; ++i) {
     corr_xy[2 * i] = X[2 * i] * Y[2 * i] + X[2 * i + 1] * Y[2 * i + 1];
     corr_xy[2 * i + 1] = X[2 * i + 1] * Y[2 * i] - X[2 * i] * Y[2 * i + 1]; 
+    smooth_corr_xy[2 * i] = 0.1 * corr_xy[2 * i] + 0.9 * smooth_corr_xy[2 * i];
+    smooth_corr_xy[2 * i + 1] = 0.1 * corr_xy[2 * i + 1] + 0.9 * smooth_corr_xy[2 * i + 1]
   }
-   // 3. 计算相干性
+   // c. 计算相干性
   for (i = 0; i < 257; ++i) {
-    tmp = corr_xy[2 * i] * corr_xy[2 * i] + corr_xy[2 * i + 1] * corr_xy[2 * i + 1];
+    tmp = smooth_corr_xy[2 * i] * smooth_corr_xy[2 * i] + smooth_corr_xy[2 * i + 1] * smooth_corr_xy[2 * i + 1];
     cohxd[i] = tmp / (X_power_smooth[i] * Y_power_smooth[i] + EPS);
   }
 
-// 6. 根据cohxd, coheec计算NLP增益
+// 7. 根据cohxd, cohxe计算NLP增益
    for (i = 0; i < 400Hz; ++i) {
-       cohxd[i] = min(cohxd[i], 1);
-       coheec[i] = min(coheec[i], 1);
-       gain = ((cohxd[i] - coheec[i]) * 1.5 + 1.0) * coheec[i];
-       gain = min(gain, 1.0);
+       gain = ((cohxd[i] - cohxe[i]) * 1.5 + 1.0) * cohxe[i];
+       gain = max(gain, cohxe[i]);
+       gain = min(gain, 0.95); // 0.95 不同频域设置不同值，频带越大设置越大
+       gain = 1 - gain;
    }
    
 
+// 8. NLMS步长更新
+  // errEst: 误差信号估计
+  // curErrPow：当前帧误差能量平滑值
+  // constErrPow: 0.98平滑系数的误差信号能量平滑值
+  // smoothErrPow: 0.85平滑系数的误差能量平滑值
+  // refPow: 参考信号能量
+  // FilterBlockNum： 滤波器块数
 
+  float mu = 0.1;
+  for (i = 0; i < 257; ++i) {
+    tmp1 = min(sqrt(constErrPow[i] / max(EPS, curErrPow[i])), 1);
+    tmp2 = refPow[i] * FilterBlockNum + smoothErrPow[i];
+    R = tmp1 * errEst[2 * i] / (tmp2 + EPS);
+    I = tmp1 * errEs[2 * i + 1] / (tmp2 + EPS);
+
+    alpha = 0.002f / max(sqrt(R * R + I * I), 0.002f);
+    R *= alpha;
+    I *= alpha;
+    
+    step[2 * i] = mu * R;
+    step[2 * i + 1] = mu * I;
+  }
+
+// 9. 基频检测
+  // mag: 幅度谱
+
+  for (i = 0; i < 257; ++i) {
+    cemp[i] = mag[2 * i] * mag[2 * i] + mag[2 * i + 1] * mag[2 * i + 1];
+  }
+  cemp = log(cemp);
+
+  for (i = 257; i >= 0; --i) {
+    cemp[2 * i] = cemp[i];
+    cemp[2 * i + 1] = 0;
+  }
+
+  cemp = IFFT(cemp)
+
+  L = Fs/400 - 1;
+  H = Fs / 70 - 1;
+
+  pitchId = -1;
+  for (L:H) {
+    if (cemp[i] >= 0.36) {
+        pitch = i;
+        break;
+    }
+  }
+
+// 10. 能量求dB
+  // energy: 能量谱
+  // [L, H]: 求dB的能量谱范围
+  
+  for (L: H) {
+    sumEnergy += energy[i];
+  }
+  coff = (H - L) * 0.5 * stft_size;
+  tmp = sumEnergy / coff;
+  dB = LOG(tmp) * 0.434294481903252;
+  dB = max(10 * dB, -20);
+
+// 11. 根据cohxd, cohxe计算NLP增益
+   for (i = 0; i < 400Hz; ++i) {
+       gain = ((cohxd[i] - cohxe[i]) * 1.5 + 1.0) * cohxe[i];
+       gain = max(gain, cohxe[i]);
+       gain = min(gain, 0.95); // 0.95 不同频域设置不同值，频带越大设置越大
+       gain = 1 - gain;
+   }
+
+ 
